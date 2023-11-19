e87367c555f2985fc984f87f5bf7446470223e5d
Alex <alex95.taylor@gmail.com>
2023-11-19 18:45:54 -0500

    Remove unnecessary line of code

--- src/todos.js
+++ src/todos.js
@@ -1,5 +1,3 @@
-const test = ['test', 'desc', 'dueDate', 'high'];
-
 const todoModule = (function() {
     function createTodo(title, desc, dueDate, priority) {
         return {title, desc, dueDate, priority};
