355fdc54f693a6ebb42f3e9cf7e331113fef8620
Alex <alex95.taylor@gmail.com>
2023-11-23 16:14:09 -0500

    Remove comments

--- src/index.js
+++ src/index.js
@@ -8,6 +8,3 @@ const test = todoModule.createTodo('test', 'desc', 'high');
 todoModule.addTodo(test);
 interfaceModule.displayTodos();
 interfaceModule.validateForm();
-
-//delete functionaily
-//fix bug where edit edits wrong
